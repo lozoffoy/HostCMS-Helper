9fb6cd69a90d1316c7e45cdd207ad22e72071749
Dmitry Lozovoy <lozoffoy@gmail.com>
2014-08-13 00:24:45 +0400

    Примеры контроллеров показа Инфосистемы и Производителей

---
+++ Informationsystem_Controller_Show.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ Контроллер показа Инфосистемы
+ */
+
+$Informationsystem_Controller_Show = new Informationsystem_Controller_Show(
+	Core_Entity::factory('Informationsystem', 1)
+);
+
+/* Сортировка элементов */
+$Informationsystem_Controller_Show
+	->informationsystemItems()
+	->queryBuilder()
+	->clearOrderBy() // очистить старые настройки сортировки
+	->orderBy('RAND()'); // добавить RAND
+
+$Informationsystem_Controller_Show
+	->xsl(
+		Core_Entity::factory('Xsl')->getByName('СписокЭлементов')
+	)
+	->groupsMode('none') 
+	->itemsForbiddenTags(array('text')) // удалить теги их XML-дерева 
+	->group(FALSE) // элементы из всех групп (либо ID группы)
+	->limit(9)
+	->show();
---
+++ Shop_Producer_Controller_Show.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ Контроллер показа производителей интернет-магазина
+ */
+
+$Shop_Producer_Controller_Show = new Shop_Producer_Controller_Show(
+	Core_Entity::factory('Shop', 3)
+);
+
+/* Сортировка производителей */
+$Shop_Producer_Controller_Show
+	->shopProducers()
+	->queryBuilder()
+	->clearOrderBy() // очистить старые настройки сортировки
+	->orderBy('RAND()'); // добавить RAND
+
+$Shop_Producer_Controller_Show
+	->xsl(
+		Core_Entity::factory('Xsl')->getByName('СписокПроизводителей')
+	)
+	->limit(6)
+	->show();

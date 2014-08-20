2ae6af42cd5d8bd107061cbad8c0d1fad428d1f7
Dmitry Lozovoy <lozoffoy@gmail.com>
2014-08-20 19:09:31 +0400

    Дополнен пример контроллера показа ИС

--- Informationsystem_Controller_Show.php
+++ Informationsystem_Controller_Show.php
@@ -14,14 +14,21 @@ $Informationsystem_Controller_Show
 	->queryBuilder()
 	->clearOrderBy() // очистить старые настройки сортировки
 	->orderBy('RAND()'); // добавить RAND
+	
+$Informationsystem_Controller_Show->addEntity(
+		Core::factory('Core_Xml_Entity')
+			->name('tag_name')->value('value')
+	);
 
 $Informationsystem_Controller_Show
 	->xsl(
 		Core_Entity::factory('Xsl')->getByName('СписокЭлементов')
 	)
-	->groupsMode('none') 
+	->groupsProperties(TRUE|FALSE|array())
 	->itemsForbiddenTags(array('text')) // удалить теги их XML-дерева 
-	->group(FALSE) // элементы из всех групп (либо ID группы)
+	->groupsMode('none'|'tree'|'all')
+	->group(FALSE|int) // элементы из всех групп (либо ID группы)
+	->item(123) // идентификатор показываемого информационного элемента
 	->limit(9)
 	->show();
 	

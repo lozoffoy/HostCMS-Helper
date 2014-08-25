f93f1f5c0aa821ddebbcf8102630bc82bc74f901
Dmitry Lozovoy <lozoffoy@gmail.com>
2014-08-25 17:26:05 +0400

    Дополнен пример контроллера показа ИС

--- Informationsystem_Controller_Show.php
+++ Informationsystem_Controller_Show.php
@@ -8,13 +8,24 @@ $Informationsystem_Controller_Show = new Informationsystem_Controller_Show(
 	Core_Entity::factory('Informationsystem', 1)
 );
 
+/* Настройка групп */
+$Informationsystem_Controller_Show
+	->groupsProperties(TRUE|FALSE|array())
+	->groupsMode('none'|'tree'|'all');
+
+/* Настройка элементов */
+$Informationsystem_Controller_Show
+	->itemsProperties(TRUE|FALSE|array())
+	->itemsForbiddenTags(array('text')); // удалить теги их XML-дерева 
+
 /* Сортировка элементов */
 $Informationsystem_Controller_Show
 	->informationsystemItems()
 	->queryBuilder()
 	->clearOrderBy() // очистить старые настройки сортировки
 	->orderBy('RAND()'); // добавить RAND
-	
+
+/* Добавить узел в XML */
 $Informationsystem_Controller_Show->addEntity(
 		Core::factory('Core_Xml_Entity')
 			->name('tag_name')->value('value')
@@ -24,9 +35,6 @@ $Informationsystem_Controller_Show
 	->xsl(
 		Core_Entity::factory('Xsl')->getByName('СписокЭлементов')
 	)
-	->groupsProperties(TRUE|FALSE|array())
-	->itemsForbiddenTags(array('text')) // удалить теги их XML-дерева 
-	->groupsMode('none'|'tree'|'all')
 	->group(FALSE|int) // элементы из всех групп (либо ID группы)
 	->item(123) // идентификатор показываемого информационного элемента
 	->limit(9)
